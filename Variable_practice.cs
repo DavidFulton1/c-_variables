e2fead1986a9cc309cfefb687800c3e98e222ff6
DavidFulton1 <fultondavids@gmail.com>
2022-07-01 11:51:09 -0500

    varaible excerecise

---
+++ Variable_practice.cs
@@ -0,0 +1,12 @@
+﻿
+string petName1 = "jack";
+int petAge1 = 7;
+char x = 'j';
+bool isBoy = true;
+double petWeight = 35.50;
+decimal yearsSpent = .5m;
+Console.WriteLine($"My dog's name is {petName1} and he is {petAge1} years old. Sometimes we call him {x} for short. {petName1} weighs {petWeight}lbs and we have had him {yearsSpent} years" );
+if (isBoy)
+{
+    Console.WriteLine("He is a good boy");
+}
